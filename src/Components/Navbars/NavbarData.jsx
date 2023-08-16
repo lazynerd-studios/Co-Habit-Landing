d50ee7e3c114e3dc8f39e69e82f75924674f78c9
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-16 13:39:44 +0100

    Create NavbarData.jsx

---
+++ src/Components/Navbars/NavbarData.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { RiArrowDropDownFill } from "react-icons/ri";
+
+export const NavbarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: "",
+    className: "nav-text",
+  },
+
+  {
+    title: "About Us",
+    path: "/about-us",
+    // icon: <RiArrowDropDownFill className="" />,
+    className: "nav-text",
+  },
+
+  {
+    title: "Listings",
+    path: "/listings",
+    icon: "",
+    className: "nav-text",
+  },
+
+  // {
+  //   title: "About us",
+  //   path: "/about-us",
+  //   icon: "",
+  //   className: "nav-text",
+  // }
+];
