43f7ea01ae939b299d9247b69c47dd41fd9c1dfa
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-16 13:39:48 +0100

    Create ScrollButton.js

---
+++ src/Components/ScrollToTop/ScrollButton.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Button = styled.div`
+position: fixed;
+padding: 0;
+width: 50px;
+height: 17%;
+right: 10px;
+bottom: 40px;
+font-size: 3rem;
+z-index: 1;
+cursor: pointer;
+color: #B7D2D2;
+transform: scale(0.7);
+`
