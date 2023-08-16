b01cf68d266f2dc365a291e35d98615373b5078c
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-16 16:01:22 +0100

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,6 +5,17 @@ const Home = () => {
     <section className="">
       <div>
 
+        <div className="hero min-h-screen bg-[#f8f8fd]">
+          <div className="hero-content text-center">
+            <div className="max-w-lg">
+              <h1 className=" text-[3.125rem] font-bold uppercase text-[#25324B]">FIND YOUR PERFECT</h1>
+              <h1 className="text-5xl font-bold uppercase text-[#010886]">Shared APARTMENT</h1>
+              <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+              <button className="btn bg-[#01088675] hover:bg-[#010886] text-white hover:text-white">Search</button>
+            </div>
+          </div>
+        </div>
+
       </div>
 
     </section>
