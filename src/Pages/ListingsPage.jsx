46207a40f7f42c58a21f0987b48a67a9d393ca63
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-16 13:39:59 +0100

    Create ListingsPage.jsx

---
+++ src/Pages/ListingsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Listings from '../Components/Listing/Listing'
+
+const ListingsPage = () => {
+  return (
+    <div>
+      <Listings />
+    </div>
+  )
+}
+
+export default ListingsPage
