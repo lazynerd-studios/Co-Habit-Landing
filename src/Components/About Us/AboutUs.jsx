55969604c6007eb25fae0a7ca22b96b8ad23f058
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-16 13:39:32 +0100

    Create AboutUs.jsx

---
+++ src/Components/About Us/AboutUs.jsx
@@ -0,0 +1,16 @@
+//<br className="hidden laptop:block" />
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <section>
+
+      <div className="">
+
+      </div>
+
+    </section>
+  )
+}
+
+export default AboutUs
