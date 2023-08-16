a33f1e1b2fe9b4454608191a4cec1be20dd85b00
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-16 14:01:20 +0100

    Update Dashboard.jsx

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbars/Navbar"
 import Footer from "../Components/Footer/Footer"
-import NewNavBar from "../Components/Navbars/NewNavBar";
 import ScrollButton from "../Components/ScrollToTop/ScrollToTop";
 
 
@@ -11,7 +10,6 @@ const Dashboard = () => {
     <section>
       <div>
         <Navbar />
-        <NewNavBar />
       </div>
 
       <div className="">
