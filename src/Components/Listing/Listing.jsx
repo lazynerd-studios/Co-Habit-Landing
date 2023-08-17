a069e5f9bb141d5068d55232ff9197b28a523aac
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-17 11:10:55 +0100

    Update Listing.jsx

--- src/Components/Listing/Listing.jsx
+++ src/Components/Listing/Listing.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { motion } from "framer-motion";
+
+const animationConfiguration = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+};
+
 
 const Listings = () => {
   return (
     <section>
-      <div className="">
-        <div className="hero min-h-screen bg-[#f8f8fd]">
+
+      <div className="hero min-h-screen bg-[#f8f8fd]">
+        <motion.div
+          variants={animationConfiguration}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{ duration: 3 }}
+        >
           <div className="hero-content text-center">
             <div className="max-w-lg">
               <h1 className=" text-[3.125rem] leading-[3.4375rem] font-bold uppercase text-[#25324B]">LISTING</h1>
@@ -12,7 +27,7 @@ const Listings = () => {
               <button className="py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">Search</button> */}
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
 
     </section>
