735ec4ad73837026396c75813397179fe8b11c9a
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-17 10:26:54 +0100

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
           <div className="hero-content text-center">
             <div className="max-w-lg">
               <h1 className=" text-[3.125rem] leading-[3.4375rem] font-bold uppercase text-[#25324B]">FIND YOUR PERFECT</h1>
-              <h1 className="text-5xl font-bold leading-[3.4375rem] uppercase text-[#010886]">Shared APARTMENT</h1>
+              <h1 className="text-5xl font-bold leading-[3.4375rem] text-[#010886]">SHARED APARTMENT</h1>
               <button className="py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">Search</button>
             </div>
           </div>
