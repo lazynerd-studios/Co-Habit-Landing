697d02ab53d0bc39191d2feff290d028ffd01c16
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-17 12:37:07 +0100

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import Left from "../../assets/left.svg"
 import Right from "../../assets/right.svg"
+import HeroImage from "../../assets/hero-image.png"
 
 const animationConfiguration = {
   initial: { opacity: 0 },
@@ -21,16 +22,17 @@ const Home = () => {
           exit="exit"
           transition={{ duration: 3 }}
         >
-          <div className="hero-content text-center">
-            <div className="w-full">
-              <h1 className=" text-[3.125rem] leading-[3.4375rem] font-bold uppercase text-[#25324B]">FIND YOUR PERFECT</h1>
+          <div className="hero-content text-center laptop:mx-[8rem]">
+            <div className="w-full laptop:mt-[6rem]">
+              <h1 className="text-[3.125rem] leading-[3.4375rem] font-bold uppercase text-[#25324B]">FIND YOUR PERFECT</h1>
               <span className="laptop:flex">
-                <img src={Left} className="laptop:mr-8 hidden laptop:flex" alt="left" />
-                <h1 className="text-5xl font-bold leading-[3.4375rem] text-[#010886]">SHARED APARTMENT</h1>
-                <img src={Right} className="laptop:ml-8 hidden laptop:flex" alt="right" />
+                <img src={Left} className=" w-[15%] hidden laptop:flex" alt="left" />
+                <h1 className="mx-auto text-5xl font-bold leading-[3.4375rem] text-[#010886]">SHARED APARTMENT</h1>
+                <img src={Right} className="w-[15%] hidden laptop:flex" alt="right" />
                 {/* mx-auto w-[30%] */}
               </span>
-              <button className="btn rounded-none normal-case py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">Search</button>
+              <img src={HeroImage} className="laptop:max-w-[80%] w-full mx-auto" alt="Hero Image" />
+              {/* <button className="btn rounded-none normal-case py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">Search</button> */}
             </div>
           </div>
         </motion.div>
