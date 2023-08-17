3593f98ef885c0b2097517159c3b950aa50344e4
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-17 11:10:50 +0100

    Update AboutUs.jsx

--- src/Components/About Us/AboutUs.jsx
+++ src/Components/About Us/AboutUs.jsx
@@ -1,12 +1,26 @@
 //<br className="hidden laptop:block" />
 import React from 'react'
+import { motion } from "framer-motion";
+
+const animationConfiguration = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+};
+
 
 const AboutUs = () => {
   return (
     <section>
 
-      <div className="">
-        <div className="hero min-h-screen bg-[#f8f8fd]">
+      <div className="hero min-h-screen bg-[#f8f8fd]">
+        <motion.div
+          variants={animationConfiguration}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{ duration: 3 }}
+        >
           <div className="hero-content text-center">
             <div className="max-w-lg">
               <h1 className=" text-[3.125rem] leading-[3.4375rem] font-bold uppercase text-[#25324B]">ABOUT US</h1>
@@ -14,7 +28,7 @@ const AboutUs = () => {
               <button className="py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">Search</button> */}
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
 
     </section>
