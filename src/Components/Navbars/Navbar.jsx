aa8a3d57e15105feca86c6b6a57f2917466f8e1d
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-17 10:34:12 +0100

    Update Navbar.jsx

--- src/Components/Navbars/Navbar.jsx
+++ src/Components/Navbars/Navbar.jsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { NavbarData } from "./NavbarData"
 import CoHabit from "../../assets/Cohabit Logo C 1.png"
 
 const Navbar = () => {
+  const [showNav, setshowNav] = useState(false)
+
   return (
     <div className="navbar bg-white fixed">
       <div className="navbar-start">
         <div className="dropdown">
           {/* hamburger menu */}
-          <label tabIndex={0} className="btn btn-ghost laptop:hidden">
+          <label onClick={() => setshowNav(!showNav)}
+            tabIndex={0} className="btn btn-ghost laptop:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
           {/* end of hamburger menu */}
           {/* mobile navbar */}
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          {showNav && <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             {NavbarData.map((item, index) => {
               return (
                 <li key={index} className="">
@@ -27,7 +30,7 @@ const Navbar = () => {
                 </li>
               );
             })}
-          </ul>
+          </ul>}
           {/* end of mobile navbar */}
         </div>
         <a>
