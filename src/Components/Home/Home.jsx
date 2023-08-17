d6b18410170318812a602a08260b3d5d0849a5c3
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-17 12:17:04 +0100

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -22,12 +22,13 @@ const Home = () => {
           transition={{ duration: 3 }}
         >
           <div className="hero-content text-center">
-            <div className="max-w-lg">
+            <div className="w-full">
               <h1 className=" text-[3.125rem] leading-[3.4375rem] font-bold uppercase text-[#25324B]">FIND YOUR PERFECT</h1>
-              <span>
-                <img src={Left} alt="left" />
+              <span className="laptop:flex">
+                <img src={Left} className="laptop:mr-8 hidden laptop:flex" alt="left" />
                 <h1 className="text-5xl font-bold leading-[3.4375rem] text-[#010886]">SHARED APARTMENT</h1>
-                <img src={Right} alt="right" />
+                <img src={Right} className="laptop:ml-8 hidden laptop:flex" alt="right" />
+                {/* mx-auto w-[30%] */}
               </span>
               <button className="btn rounded-none normal-case py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">Search</button>
             </div>
