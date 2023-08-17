77ea4e6cc6e126d4b99dee54d5427395d2837d72
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-17 10:26:56 +0100

    Update Listing.jsx

--- src/Components/Listing/Listing.jsx
+++ src/Components/Listing/Listing.jsx
@@ -4,7 +4,15 @@ const Listings = () => {
   return (
     <section>
       <div className="">
-
+        <div className="hero min-h-screen bg-[#f8f8fd]">
+          <div className="hero-content text-center">
+            <div className="max-w-lg">
+              <h1 className=" text-[3.125rem] leading-[3.4375rem] font-bold uppercase text-[#25324B]">LISTING</h1>
+              {/* <h1 className="text-5xl font-bold leading-[3.4375rem] text-[#010886]">SHARED APARTMENT</h1>
+              <button className="py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">Search</button> */}
+            </div>
+          </div>
+        </div>
       </div>
 
     </section>
