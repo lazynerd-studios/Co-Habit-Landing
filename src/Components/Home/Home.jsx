5a80a7425528f031f3cbc22afd5b69a0205e6af1
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-17 10:15:40 +0100

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -20,10 +20,9 @@ const Home = () => {
         <div className="hero min-h-screen bg-[#f8f8fd]">
           <div className="hero-content text-center">
             <div className="max-w-lg">
-              <h1 className=" text-[3.125rem] font-bold uppercase text-[#25324B]">FIND YOUR PERFECT</h1>
-              <h1 className="text-5xl font-bold uppercase text-[#010886]">Shared APARTMENT</h1>
-              <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-              <button className="btn bg-[#010886f1] hover:bg-[#010886] hover:scale-105 text-white hover:text-white">Search</button>
+              <h1 className=" text-[3.125rem] leading-[3.4375rem] font-bold uppercase text-[#25324B]">FIND YOUR PERFECT</h1>
+              <h1 className="text-5xl font-bold leading-[3.4375rem] uppercase text-[#010886]">Shared APARTMENT</h1>
+              <button className="py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">Search</button>
             </div>
           </div>
         </div>
