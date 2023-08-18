9a65e6d5e31f0bd682fc46385040a658f6739348
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-18 15:18:42 +0100

    Create slides.jsx

---
+++ src/Components/Slideshow/slides.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Bungalow1 from "../../assets/bungalow-1.png";
+import Bungalow2 from "../../assets/bungalow-2.png";
+import Bungalow3 from "../../assets/bungalow-3.png";
+import BathIcon from "../../assets/u_bath.svg"
+import BedIcon from "../../assets/u_bed-double.svg"
+
+export const slides = [
+    {
+        image: Bungalow1,
+        house: "Bungalow",
+        price: "NGN150,000",
+    },
+    {
+        image: Bungalow2,
+        house: "Bungalow",
+        price: "NGN150,000",
+    },
+    {
+        image: Bungalow3,
+        house: "Bungalow",
+        price: "NGN150,000",
+    },
+    {
+        image: Bungalow1,
+        house: "Bungalow",
+        price: "NGN150,000",
+    },
+    {
+        image: Bungalow2,
+        house: "Bungalow",
+        price: "NGN150,000",
+    },
+    {
+        image: Bungalow3,
+        house: "Bungalow",
+        price: "NGN150,000",
+    },
+];
