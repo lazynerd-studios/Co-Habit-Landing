e0d817c3b5ab4b48e0d3f339374cb6a7b8f76dd8
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-18 14:12:31 +0100

    Update Slideshow.jsx

--- src/Components/Slideshow/Slideshow.jsx
+++ src/Components/Slideshow/Slideshow.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
-import Bungalow1 from "../../assets/bungalow-1.png"
-import Bungalow2 from "../../assets/bungalow-2.png"
-import Bungalow3 from "../../assets/bungalow-3.png"
-import BathIcon from "../../assets/u_bath.svg"
-import BedIcon from "../../assets/u_bed-double.svg"
-import "./styles.css"
+import React, { useState, useEffect, useRef } from "react";
+import Bungalow1 from "../../assets/bungalow-1.png";
+import Bungalow2 from "../../assets/bungalow-2.png";
+import Bungalow3 from "../../assets/bungalow-3.png";
+import "./styles.css";
 
-const colors = ["#0088FE", "#00C49F", "#FFBB28"];
+const slides = [
+    { image: Bungalow1, house: "Bungalow" },
+    { image: Bungalow2, house: "Bungalow" },
+    { image: Bungalow3, house: "Bungalow" }
+];
 const delay = 2500;
 
 function Slideshow() {
     const [index, setIndex] = useState(0);
-    const timeoutRef = React.useRef(null);
+    const timeoutRef = useRef(null);
 
     function resetTimeout() {
         if (timeoutRef.current) {
@@ -19,39 +21,30 @@ function Slideshow() {
         }
     }
 
-    React.useEffect(() => {
+    useEffect(() => {
         resetTimeout();
-        timeoutRef.current = setTimeout(
-            () =>
-                setIndex((prevIndex) =>
-                    prevIndex === colors.length - 1 ? 0 : prevIndex + 1
-                ),
-            delay
-        );
+        timeoutRef.current = setTimeout(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % slides.length);
+        }, delay);
 
         return () => {
             resetTimeout();
         };
     }, [index]);
 
+    const currentSlide = slides[index];
+
     return (
         <div className="laptop:hidden block overflow-hidden w-[95%] mx-auto mt-8">
-            <div
-                className="slideshowSlider whitespace-nowrap"
-                style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
-            >
-                {colors.map((backgroundColor, index) => (
-                    <img
-                        className="inline-block w-auto rounded-t-[1rem]"
-                        key={index}
-                        // style={{ backgroundColor }}
-                        src={Bungalow3}
-                    />
-                ))}
+            <div className="slideshowSlider">
+                <div className="slide">
+                    <img className="inline-block w-auto rounded-t-[1rem]" src={currentSlide.image} alt={`Slide ${index + 1}`} />
+                    <p>{currentSlide.house}</p>
+                </div>
             </div>
 
             <div className="slideshowDots text-center">
-                {colors.map((_, idx) => (
+                {slides.map((_, idx) => (
                     <div
                         key={idx}
                         className={`slideshowDot${index === idx ? " bg-[#010886]" : ""}`}
