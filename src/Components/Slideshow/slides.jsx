a28e7b259feaab52ae1e43ea8483ceefdcabff7f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-18 15:56:03 +0100

    Update slides.jsx

--- src/Components/Slideshow/slides.jsx
+++ src/Components/Slideshow/slides.jsx
@@ -2,38 +2,60 @@ import React from "react";
 import Bungalow1 from "../../assets/bungalow-1.png";
 import Bungalow2 from "../../assets/bungalow-2.png";
 import Bungalow3 from "../../assets/bungalow-3.png";
-import BathIcon from "../../assets/u_bath.svg"
-import BedIcon from "../../assets/u_bed-double.svg"
 
 export const slides = [
     {
         image: Bungalow1,
         house: "Bungalow",
         price: "NGN150,000",
+        measurement: "1200sqft",
+        rooms: "2 Rooms",
+        baths: "2 Baths",
+        area: "Surulere, Lagos"
     },
     {
         image: Bungalow2,
         house: "Bungalow",
         price: "NGN150,000",
+        measurement: "1300sqft",
+        rooms: "3 Rooms",
+        baths: "3 Baths",
+        area: "Ikorodu, Lagos"
     },
     {
         image: Bungalow3,
         house: "Bungalow",
         price: "NGN150,000",
+        measurement: "1300sqft",
+        rooms: "2 Rooms",
+        baths: "2 Baths",
+        area: "Ikoyi, Lagos"
     },
     {
         image: Bungalow1,
         house: "Bungalow",
         price: "NGN150,000",
+        measurement: "1400sqft",
+        rooms: "3 Rooms",
+        baths: "3 Baths",
+        area: "Ikeja, Lagos"
     },
     {
         image: Bungalow2,
         house: "Bungalow",
         price: "NGN150,000",
+        measurement: "1200sqft",
+        rooms: "2 Rooms",
+        baths: "2 Baths",
+        area: "Epe, Lagos"
     },
     {
         image: Bungalow3,
         house: "Bungalow",
         price: "NGN150,000",
+        measurement: "1500sqft",
+        rooms: "3 Rooms",
+        baths: "3 Baths",
+        area: "Apapa, Lagos"
     },
 ];
