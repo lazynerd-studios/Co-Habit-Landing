9b1e5ad1e6555baed272fc58e93537e957685dbf
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-18 16:21:56 +0100

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -121,21 +121,21 @@ const Home = () => {
               {/* gap-16 */}
               {/* styled rectangle 1 */}
               <div className="">
-                <p className="absolute w-[68%] ml-[3.5rem] mt-[9rem] laptop:ml-[1.1rem] laptop:mt-[10.5rem] text-[0.875rem] font-medium">
+                <p className="absolute w-[68%] ml-[3.5rem] mt-[8.3rem] laptop:ml-[1.1rem] laptop:mt-[10.5rem] text-[0.875rem] font-medium">
                   The apartment appeared to be well-cared- <br className="laptop:flex hidden" />
                   for and professionally managed. The <br className="laptop:flex hidden" />
                   overall maintenance of the house and the <br className="laptop:flex hidden" />
                   surrounding landscape reflected a high <br className="laptop:flex hidden" />
                   level of attention to detail.
                 </p>
-                <h2 className="absolute w-auto ml-[3.5rem] mt-[6.7rem] laptop:ml-[1.1rem] laptop:mt-[8rem] text-[1.125rem] font-medium">
+                <h2 className="absolute w-auto ml-[3.5rem] mt-[6.3rem] laptop:ml-[1.1rem] laptop:mt-[8rem] text-[1.125rem] font-medium">
                   List your property
                 </h2>
-                <p className="absolute text-[1rem] font-medium ml-[18.45rem] mt-[1.9rem] laptop:ml-[16.5rem] z-10 laptop:mt-[1.6rem]">
+                <p className="absolute text-[1rem] font-medium ml-[17.5rem] mt-[1.5rem] laptop:ml-[16.5rem] z-10 laptop:mt-[1.6rem]">
                   1
                 </p>
                 <img src={LocationPin} className="absolute laptop:w-[3rem] w-[10%] ml-[3rem] mt-[3.5rem] laptop:ml-[.5rem] laptop:mt-[3.5rem]" alt="location-pin" />
-                <img src={SmallStyledRectangle} className="absolute w-[17%] laptop:w-[4.5%] ml-[16.8rem] mt-[0.06rem] laptop:ml-[15rem] laptop:mt-[0.08rem]" alt="small-styled-rectangle" />
+                <img src={SmallStyledRectangle} className="absolute w-[14%] laptop:w-[4.5%] ml-[16rem] mt-[0.19rem] laptop:ml-[15rem] laptop:mt-[0.08rem]" alt="small-styled-rectangle" />
                 <img src={StyledRectangle} className="laptop:w-[20rem] w-[80%] mx-auto my-4 laptop:my-auto" alt="styled-rectangle" />
               </div>
               {/* end of styled rectangle 1 */}
@@ -145,21 +145,21 @@ const Home = () => {
 
               {/* styled rectangle 2 */}
               <div className="">
-                <p className="absolute w-[68%] ml-[3.5rem] mt-[9rem] laptop:ml-[1.1rem] laptop:mt-[10.5rem] text-[0.875rem] font-medium">
+                <p className="absolute w-[68%] ml-[3.5rem] mt-[8.3rem] laptop:ml-[1.1rem] laptop:mt-[10.5rem] text-[0.875rem] font-medium">
                   The apartment appeared to be well-cared- <br className="laptop:flex hidden" />
                   for and professionally managed. The <br className="laptop:flex hidden" />
                   overall maintenance of the house and the <br className="laptop:flex hidden" />
                   surrounding landscape reflected a high <br className="laptop:flex hidden" />
                   level of attention to detail.
                 </p>
-                <h2 className="absolute w-auto ml-[3.5rem] mt-[6.7rem] laptop:ml-[1.1rem] laptop:mt-[8rem] text-[1.125rem] font-medium">
+                <h2 className="absolute w-auto ml-[3.5rem] mt-[6.3rem] laptop:ml-[1.1rem] laptop:mt-[8rem] text-[1.125rem] font-medium">
                   Find Your Flatmate
                 </h2>
-                <p className="absolute text-[1rem] font-medium ml-[18.45rem] mt-[1.9rem] laptop:ml-[16.5rem] z-10 laptop:mt-[1.6rem]">
+                <p className="absolute text-[1rem] font-medium ml-[17.5rem] mt-[1.5rem] laptop:ml-[16.5rem] z-10 laptop:mt-[1.6rem]">
                   2
                 </p>
                 <img src={Users} className="absolute laptop:w-[3rem] w-[10%] ml-[3.5rem] mt-[3.5rem] laptop:ml-[1rem] laptop:mt-[3.5rem]" alt="location-pin" />
-                <img src={SmallStyledRectangle} className="absolute w-[17%] laptop:w-[4.5%] ml-[16.8rem] mt-[0.06rem] laptop:ml-[15rem] laptop:mt-[0.08rem]" alt="small-styled-rectangle" />
+                <img src={SmallStyledRectangle} className="absolute w-[14%] laptop:w-[4.5%] ml-[16rem] mt-[0.19rem] laptop:ml-[15rem] laptop:mt-[0.08rem]" alt="small-styled-rectangle" />
                 <img src={StyledRectangle} className="laptop:w-[20rem] w-[80%] mx-auto my-4 laptop:my-auto" alt="styled-rectangle" />
               </div>
 
@@ -168,21 +168,21 @@ const Home = () => {
 
               {/* styled rectangle 3 */}
               <div className="">
-                <p className="absolute w-[68%] ml-[3.5rem] mt-[9rem] laptop:ml-[1.1rem] laptop:mt-[10.5rem] text-[0.875rem] font-medium">
+                <p className="absolute w-[68%] ml-[3.5rem] mt-[8.3rem] laptop:ml-[1.1rem] laptop:mt-[10.5rem] text-[0.875rem] font-medium">
                   The apartment appeared to be well-cared- <br className="laptop:flex hidden" />
                   for and professionally managed. The <br className="laptop:flex hidden" />
                   overall maintenance of the house and the <br className="laptop:flex hidden" />
                   surrounding landscape reflected a high <br className="laptop:flex hidden" />
                   level of attention to detail.
                 </p>
-                <h2 className="absolute w-auto ml-[3.5rem] mt-[6.7rem] laptop:ml-[1.1rem] laptop:mt-[8rem] text-[1.125rem] font-medium">
+                <h2 className="absolute w-auto ml-[3.5rem] mt-[6.3rem] laptop:ml-[1.1rem] laptop:mt-[8rem] text-[1.125rem] font-medium">
                   Close The Deal
                 </h2>
-                <p className="absolute text-[1rem] font-medium ml-[18.45rem] mt-[1.9rem] laptop:ml-[16.5rem] z-10 laptop:mt-[1.6rem]">
+                <p className="absolute text-[1rem] font-medium ml-[17.5rem] mt-[1.5rem] laptop:ml-[16.5rem] z-10 laptop:mt-[1.6rem]">
                   3
                 </p>
                 <img src={MoneyStack} className="absolute laptop:w-[3rem] w-[10%] ml-[3.5rem] mt-[3.5rem] laptop:ml-[1rem] laptop:mt-[3.5rem]" alt="location-pin" />
-                <img src={SmallStyledRectangle} className="absolute w-[17%] laptop:w-[4.5%] ml-[16.8rem] mt-[0.06rem] laptop:ml-[15rem] laptop:mt-[0.08rem]" alt="small-styled-rectangle" />
+                <img src={SmallStyledRectangle} className="absolute w-[14%] laptop:w-[4.5%] ml-[16rem] mt-[0.19rem] laptop:ml-[15rem] laptop:mt-[0.08rem]" alt="small-styled-rectangle" />
                 <img src={StyledRectangle} className="laptop:w-[20rem] w-[80%] mx-auto my-4 laptop:my-auto" alt="styled-rectangle" />
               </div>
             </div>
