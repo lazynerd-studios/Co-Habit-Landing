701acc7d5134820da7e9561f916811e9005f21fd
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-21 13:24:28 +0100

    Update Navbar.jsx

--- src/Components/Navbars/Navbar.jsx
+++ src/Components/Navbars/Navbar.jsx
@@ -41,6 +41,7 @@ const Navbar = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
           {/* end of hamburger menu */}
+
           {/* mobile navbar */}
           {showNav && <ul tabIndex={0} className="dropdown-content mt-3 z-[1] p-2 bg-white focus:bg-white rounded-box w-52">
             {NavbarData.map((item, index) => {
