dcf775638c3726e998682cbddf12709688cd34a6
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-23 12:39:35 +0100

    Update Listing.jsx

--- src/Components/Listing/Listing.jsx
+++ src/Components/Listing/Listing.jsx
@@ -16,19 +16,19 @@ const animationConfiguration = {
 
 const Listings = () => {
   return (
-    <section className='w-full'>
+    <section className=''>
+      <motion.div
+        variants={animationConfiguration}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ duration: 3 }}
+      >
+        <div className="hero min-h-screen bg-[#f8f8fd]">
 
-      <div className="hero min-h-screen bg-[#f8f8fd]">
-        <motion.div
-          variants={animationConfiguration}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          transition={{ duration: 3 }}
-        >
-          <div className="hero-content text-center laptop:mt-auto laptop:mx-[8rem]">
+          <div className=" laptop:mt-auto laptop:mx-[8rem]">
 
-            <div className="w-auto">
+            <div className="text-center">
 
               <h1 className=" text-[2.125rem] laptop:leading-[3.3rem] font-bold text-[#25324B]">
                 <span className="text-5xl">
@@ -49,74 +49,74 @@ const Listings = () => {
               </p>
               {/* <button className="py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">Search</button> */}
             </div>
-          </div>
 
+            {/* search */}
+            <div className="mt-[] laptop: laptop:flex justify-between laptop:mx-[] rounded-md bg-white p-2 px-4 border-2 border-none">
+              {/* laptop:w-[65%] laptop:mx-[5rem] */}
+              <span className="flex">
 
-          {/* search */}
-          <div className="mt-[] laptop: laptop:flex justify-between laptop:mx-[] rounded-md bg-white p-2 px-4 border-2 border-none">
-            {/* laptop:w-[65%] laptop:mx-[5rem] */}
-            <span className="flex">
+                <span className="my-3 mx-2">
+                  <BiSearch size={25} />
+                </span>
 
-              <span className="my-3 mx-2">
-                <BiSearch size={25} />
+                <input type="text"
+                  className="border-b-[#7C8493] w-full  mr-[2rem] laptop:px-0 px-2 py-2 border-2 border-t-white border-x-white outline-none text-[#7C8493]"
+                  placeholder="Apartment/Workspace"
+                  autoComplete="true"
+                  size={40}
+                />
               </span>
 
-              <input type="text"
-                className="border-b-[#7C8493] w-full  mr-[2rem] laptop:px-0 px-2 py-2 border-2 border-t-white border-x-white outline-none text-[#7C8493]"
-                placeholder="Apartment/Workspace"
-                autoComplete="true"
-                size={40}
-              />
-            </span>
-
-            {/* <span className=' rotate-90 px-2'>
+              {/* <span className=' rotate-90 px-2'>
               <IoRemoveOutline size={25} />
             </span> */}
 
-            {/* location */}
-            <span className="flex ">
+              {/* location */}
+              <span className="flex ">
 
-              <span className="my-3 mx-2">
-                <GrLocation size={25} />
+                <span className="my-3 mx-2">
+                  <GrLocation size={25} />
+                </span>
+
+                {/* <img src={Location} className="laptop:w-[13%] p-1 mr-1" alt="location" /> */}
+                <select
+                  className="laptop:w-full pr-[12rem] w-[78%] py-2 border-b-[#7C8493] border-2 border-t-white border-x-white text-[#7C8493]"
+                  name="Location">
+                  <option className="py-4" value='Apartment/Workspace'>
+                    Lagos/Abuja
+                  </option>
+                  <option value='Apartment/Workspace'>
+                    Ogun/Oyo
+                  </option>
+                  <option value='Apartment/Workspace'>
+                    Port-Harcourt/Delta
+                  </option>
+                  <option value='Apartment/Workspace'>
+                    Ibadan/Abuja
+                  </option>
+                  <option value='Apartment/Workspace'>
+                    Edo/Minna
+                  </option>
+                </select>
               </span>
 
-              {/* <img src={Location} className="laptop:w-[13%] p-1 mr-1" alt="location" /> */}
-              <select
-                className="laptop:w-full pr-[12rem] w-[78%] py-2 border-b-[#7C8493] border-2 border-t-white border-x-white text-[#7C8493]"
-                name="Location">
-                <option className="py-4" value='Apartment/Workspace'>
-                  Lagos/Abuja
-                </option>
-                <option value='Apartment/Workspace'>
-                  Ogun/Oyo
-                </option>
-                <option value='Apartment/Workspace'>
-                  Port-Harcourt/Delta
-                </option>
-                <option value='Apartment/Workspace'>
-                  Ibadan/Abuja
-                </option>
-                <option value='Apartment/Workspace'>
-                  Edo/Minna
-                </option>
-              </select>
-            </span>
-
-            {/* search button */}
-            <button
-              className="btn laptop:ml-4 ml-4 rounded-none w-[80%] mt-3 laptop:w-auto laptop:mt-auto normal-case py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">
-              Search
-            </button>
+              <button
+                className="btn laptop:ml-4 ml-4 rounded-none w-[80%] mt-3 laptop:w-auto laptop:mt-auto normal-case py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">
+                Search
+              </button>
+            </div>
+            {/* end of search */}
+            <p className="text-[#515B6F] py-4 mx-5">
+              Popular : Apartment, Bungalow, 2 Bedroom
+            </p>
           </div>
-          {/* end of search */}
 
-          <p className="text-[#515B6F] py-4 mx-5">
-            Popular : Apartment, Bungalow, 2 Bedroom
-          </p>
+
+
 
 
           {/* sidebar */}
-          <div className="bg-white p-8 w-full">
+          {/* <div className="bg-white p-8 w-full">
             <span className="flex ">
               <p className="">
                 Property Type
@@ -125,13 +125,14 @@ const Listings = () => {
                 <BsChevronUp />
               </span>
             </span>
-          </div>
+          </div> */}
 
 
-        </motion.div>
-      </div>
 
-    </section>
+        </div>
+
+      </motion.div>
+    </section >
   )
 }
 
