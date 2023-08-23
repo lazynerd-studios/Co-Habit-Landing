8710991e1c05e19207e4348af90344af83d2f396
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-23 14:39:36 +0100

    Update Listing.jsx

--- src/Components/Listing/Listing.jsx
+++ src/Components/Listing/Listing.jsx
@@ -4,7 +4,9 @@ import { BiSearch } from "react-icons/bi";
 import { GrLocation } from "react-icons/gr";
 import { IoRemoveOutline } from "react-icons/io5";
 import { BsChevronUp } from "react-icons/bs";
+import { BsChevronDown } from "react-icons/bs";
 import Scribble from "../../assets/scribble-lines.svg"
+import DivideLine from "../../assets/line-Divider.png"
 
 
 const animationConfiguration = {
@@ -107,7 +109,7 @@ const Listings = () => {
             </div>
             {/* end of search */}
 
-            <p className="text-[#515B6F] py-4 mx-1">
+            <p className="text-[#515B6F] py-4 laptop:mx-1 mx-8 w-auto">
               Popular : Apartment, Bungalow, 2 Bedroom
             </p>
           </div>
@@ -115,10 +117,10 @@ const Listings = () => {
         </div>
 
         {/* listings */}
-        <div className="bg-white p-8 w-full">
+        <div className="bg-white flex p-8 w-full">
 
           {/* sidebar */}
-          <div className='laptop:block flex'>
+          <div className='laptop:block'>
             {/* 01 */}
             <section className=''>
               <span className="flex mb-6 font-semibold text-[1rem] leading-[1.6rem]">
@@ -372,7 +374,7 @@ const Listings = () => {
                 </span>
 
                 <span className='block'>
-                  {/* <input type="checkbox"
+                  <input type="checkbox"
                     id="3m-above"
                     name=""
                     value=""
@@ -384,9 +386,8 @@ const Listings = () => {
                   <label for="3m-above"
                     className="text-[#515B6F] px-4">
                     3m or above (4)
-                  </label> */}
-                  <input id="vue-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-blue-800 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-900 dark:border-blue-500" />
-                  <label for="vue-checkbox-list" class="w-full py-3 ml-2 text-sm font-semibold text-black dark:text-black">Vue JS</label>
+                  </label>
+
                 </span>
               </form>
             </section>
@@ -394,6 +395,60 @@ const Listings = () => {
           </div>
           {/* end of sidebar */}
 
+          {/* all property */}
+          <div className="">
+            <p className="text-[#25324B] font-bold text-[1.5rem] leading-[1.8rem]">
+              All Property
+            </p>
+            <span className="flex justify-between w-full">
+              <p className="text-[#25324B] font-normal text-base leading-[1.6rem]">
+                Showing 73 results
+              </p>
+              <p className="text-[#7C8493] font-normal text-base leading-[1.6rem]">
+                Sort by:
+              </p>
+              <p className="text-[#25324B] font-medium text-base leading-[1.6rem]">
+                Most relevant
+              </p>
+              <span className="py-1 px-2">
+                <BsChevronDown />
+              </span>
+              <img src={DivideLine} className="" alt='' />
+              <span className="">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                  <g clip-path="url(#clip0_1014_972)">
+                    <path d="M9 4.49998H5C4.44772 4.49998 4 4.9477 4 5.49998V9.49998C4 10.0523 4.44772 10.5 5 10.5H9C9.55228 10.5 10 10.0523 10 9.49998V5.49998C10 4.9477 9.55228 4.49998 9 4.49998Z" fill="#010886" stroke="#010886" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M19 4.49998H15C14.4477 4.49998 14 4.9477 14 5.49998V9.49998C14 10.0523 14.4477 10.5 15 10.5H19C19.5523 10.5 20 10.0523 20 9.49998V5.49998C20 4.9477 19.5523 4.49998 19 4.49998Z" fill="#010886" stroke="#010886" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M9 14.5H5C4.44772 14.5 4 14.9477 4 15.5V19.5C4 20.0523 4.44772 20.5 5 20.5H9C9.55228 20.5 10 20.0523 10 19.5V15.5C10 14.9477 9.55228 14.5 9 14.5Z" fill="#010886" stroke="#010886" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M19 14.5H15C14.4477 14.5 14 14.9477 14 15.5V19.5C14 20.0523 14.4477 20.5 15 20.5H19C19.5523 20.5 20 20.0523 20 19.5V15.5C20 14.9477 19.5523 14.5 19 14.5Z" fill="#010886" stroke="#010886" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_1014_972">
+                      <rect width="24" height="24" fill="white" transform="translate(0 0.5)" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </span>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                  <g opacity="0.5" clip-path="url(#clip0_1014_854)">
+                    <path d="M18 4.49998H6C4.89543 4.49998 4 5.39542 4 6.49998V8.49998C4 9.60455 4.89543 10.5 6 10.5H18C19.1046 10.5 20 9.60455 20 8.49998V6.49998C20 5.39542 19.1046 4.49998 18 4.49998Z" stroke="#515B6F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M18 14.5H6C4.89543 14.5 4 15.3954 4 16.5V18.5C4 19.6046 4.89543 20.5 6 20.5H18C19.1046 20.5 20 19.6046 20 18.5V16.5C20 15.3954 19.1046 14.5 18 14.5Z" stroke="#515B6F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_1014_854">
+                      <rect width="24" height="24" fill="white" transform="translate(0 0.5)" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </span>
+            </span>
+
+            <div className="">
+
+            </div>
+          </div>
+          {/* end of all property */}
 
         </div>
         {/* end of listings */}
