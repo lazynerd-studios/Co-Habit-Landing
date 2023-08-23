5dfbbd675ff648e0c582be195596e2e9586c4e13
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-23 10:16:24 +0100

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from "../Components/Home/Home"
+import NotFoundPage from '../Components/404 Page/NotFoundPage'
 
 const HomePage = () => {
   return (
     <div>
       <Home />
+      {/* <NotFoundPage /> */}
     </div>
   )
 }
