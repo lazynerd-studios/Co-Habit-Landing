a5d393b7e815c45f5d49460bd1d68805440f536f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-23 10:01:29 +0100

    Update NotFoundPage.jsx

--- src/Components/404 Page/NotFoundPage.jsx
+++ src/Components/404 Page/NotFoundPage.jsx
@@ -7,6 +7,10 @@ const NotFoundPage = () => {
             <h1 className=" text-4xl">
                 404 Page NotFound
             </h1>
+            <p>
+                The page you are looking for does not exist.
+            </p>
+
         </div>
     )
 }
