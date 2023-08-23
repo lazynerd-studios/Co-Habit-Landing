c56f5a9f8ca55f7e05fd16b332df6888de4f53b4
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-23 10:43:14 +0100

    Update Listing.jsx

--- src/Components/Listing/Listing.jsx
+++ src/Components/Listing/Listing.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { motion } from "framer-motion";
+import Search from "../../assets/Search.svg"
+import Location from "../../assets/Location.svg"
 
 
 const animationConfiguration = {
@@ -44,6 +46,54 @@ const Listings = () => {
               {/* <button className="py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">Search</button> */}
             </div>
           </div>
+
+
+          {/* search */}
+          <div className="w-full mt-[] laptop: laptop:flex laptop:mx-[] laptop:w- rounded-md bg-white px-2 py-2 border-none">
+            {/* laptop:w-[65%] laptop:mx-[5rem] */}
+            <span className="flex">
+              <img src={Search} className="laptop:w-[13%] p-1 mr-1" alt="search" />
+              <input type="text"
+                className="border-b-[#7C8493] laptop:w-auto w-[77%] mr-[2rem] p-2 border-2 border-t-white border-x-white text-[#7C8493]"
+                placeholder="Apartment/Workspace"
+                autoComplete="true"
+              />
+            </span>
+
+            {/* location */}
+            <span className="flex ">
+              <img src={Location} className="laptop:w-[13%] p-1 mr-1" alt="location" />
+              <select className="laptop:w-full laptop:mr-[] w-[78%] p-2 border-b-[#7C8493] border-2 border-t-white border-x-white text-[#7C8493]"
+                name="Location">
+                <option className="py-4" value='Apartment/Workspace'>
+                  Lagos/Abuja
+                </option>
+                <option value='Apartment/Workspace'>
+                  Ogun/Oyo
+                </option>
+                <option value='Apartment/Workspace'>
+                  Port-Harcourt/Delta
+                </option>
+                <option value='Apartment/Workspace'>
+                  Ibadan/Abuja
+                </option>
+                <option value='Apartment/Workspace'>
+                  Edo/Minna
+                </option>
+              </select>
+            </span>
+
+            {/* search button */}
+            <button
+              className="btn laptop:ml-4 -ml-4 rounded-none w-[80%] mt-3 laptop:w-auto laptop:mt-auto normal-case py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">
+              Search
+            </button>
+          </div>
+          {/* end of search */}
+
+
+
+
         </motion.div>
       </div>
 
