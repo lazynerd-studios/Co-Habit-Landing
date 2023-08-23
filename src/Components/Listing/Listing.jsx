74735d10882840d61bc5c41fc961a944a014f1e0
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-23 12:32:09 +0100

    Update Listing.jsx

--- src/Components/Listing/Listing.jsx
+++ src/Components/Listing/Listing.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { motion } from "framer-motion";
-import Search from "../../assets/Search.svg"
-import { BiSearch } from 'react-icons/fa';
-import Location from "../../assets/Location.svg"
+import { BiSearch } from "react-icons/bi";
+import { GrLocation } from "react-icons/gr";
+import { IoRemoveOutline } from "react-icons/io5";
+import { BsChevronUp } from "react-icons/bs";
 import Scribble from "../../assets/scribble-lines.svg"
 
 
@@ -15,7 +16,7 @@ const animationConfiguration = {
 
 const Listings = () => {
   return (
-    <section>
+    <section className='w-full'>
 
       <div className="hero min-h-screen bg-[#f8f8fd]">
         <motion.div
@@ -29,7 +30,7 @@ const Listings = () => {
 
             <div className="w-auto">
 
-              <h1 className="laptop:text-[3.125rem] text-[2.125rem] laptop:leading-[3.3rem] font-bold text-[#25324B]">
+              <h1 className=" text-[2.125rem] laptop:leading-[3.3rem] font-bold text-[#25324B]">
                 <span className="text-5xl">
                   Find your &nbsp;
                 </span>
@@ -38,7 +39,7 @@ const Listings = () => {
                     Shared Apartment
                   </span>
                   <img src={Scribble}
-                    className=''
+                    className='absolute laptop:mx-[18rem] mx-8 laptop:w-auto w-[80%]'
                     alt='scribble-lines' />
                 </span>
               </h1>
@@ -52,26 +53,36 @@ const Listings = () => {
 
 
           {/* search */}
-          <div className="mt-[] laptop: laptop:flex justify-between laptop:mx-[] rounded-md bg-white p-2 border-2 border-none">
+          <div className="mt-[] laptop: laptop:flex justify-between laptop:mx-[] rounded-md bg-white p-2 px-4 border-2 border-none">
             {/* laptop:w-[65%] laptop:mx-[5rem] */}
             <span className="flex">
 
-              {/* <img src={Search} className="laptop:w-[13%] p-1 mr-1" alt="search" /> */}
-              <BiSearch />
+              <span className="my-3 mx-2">
+                <BiSearch size={25} />
+              </span>
 
               <input type="text"
                 className="border-b-[#7C8493] w-full  mr-[2rem] laptop:px-0 px-2 py-2 border-2 border-t-white border-x-white outline-none text-[#7C8493]"
                 placeholder="Apartment/Workspace"
                 autoComplete="true"
-                size={50}
+                size={40}
               />
             </span>
 
+            {/* <span className=' rotate-90 px-2'>
+              <IoRemoveOutline size={25} />
+            </span> */}
+
             {/* location */}
             <span className="flex ">
-              <img src={Location} className="laptop:w-[13%] p-1 mr-1" alt="location" />
+
+              <span className="my-3 mx-2">
+                <GrLocation size={25} />
+              </span>
+
+              {/* <img src={Location} className="laptop:w-[13%] p-1 mr-1" alt="location" /> */}
               <select
-                className="laptop:w-full laptop:mr-[] w-[78%] p-2 border-b-[#7C8493] border-2 border-t-white border-x-white text-[#7C8493]"
+                className="laptop:w-full pr-[12rem] w-[78%] py-2 border-b-[#7C8493] border-2 border-t-white border-x-white text-[#7C8493]"
                 name="Location">
                 <option className="py-4" value='Apartment/Workspace'>
                   Lagos/Abuja
@@ -93,16 +104,28 @@ const Listings = () => {
 
             {/* search button */}
             <button
-              className="btn laptop:ml-4 -ml-4 rounded-none w-[80%] mt-3 laptop:w-auto laptop:mt-auto normal-case py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">
+              className="btn laptop:ml-4 ml-4 rounded-none w-[80%] mt-3 laptop:w-auto laptop:mt-auto normal-case py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">
               Search
             </button>
           </div>
           {/* end of search */}
 
-          <p className="text-[#515B6F] py-4">
+          <p className="text-[#515B6F] py-4 mx-5">
             Popular : Apartment, Bungalow, 2 Bedroom
           </p>
 
+
+          {/* sidebar */}
+          <div className="bg-white p-8 w-full">
+            <span className="flex ">
+              <p className="">
+                Property Type
+              </p>
+              <span className="px-4">
+                <BsChevronUp />
+              </span>
+            </span>
+          </div>
 
 
         </motion.div>
