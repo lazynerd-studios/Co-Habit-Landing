c3ff6f80dc1a5717e8be0c08bf9bd4d511ad4a66
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-23 17:16:54 +0100

    Update Listing.jsx

--- src/Components/Listing/Listing.jsx
+++ src/Components/Listing/Listing.jsx
@@ -39,7 +39,7 @@ const Listings = () => {
         {/* bg-[#f8f8fd] */}
         <div className="hero laptop:min-h-[screen] min-h-screen bg-header-image laptop:bg-contain bg-contain">
 
-          <div className="laptop:mt-0 laptop:mx-[8rem]">
+          <div className="laptop:mt-0 laptop:mx-[2rem]">
 
             <div className="text-center">
               <h1 className=" text-[2.125rem] laptop:leading-[3.3rem] font-bold text-[#25324B]">
@@ -127,7 +127,7 @@ const Listings = () => {
         </div>
 
         {/* listings */}
-        <div className="bg-white flex py-8 px-4  laptop:mx-[8rem]">
+        <div className="bg-white flex py-8 px-4  laptop:mx-[7rem]">
 
           {/* sidebar */}
           <div className='laptop:block  hidden'>
@@ -407,66 +407,70 @@ const Listings = () => {
 
 
           {/* all property */}
-          <div className="laptop:px-8 px-0  laptop: mx- mx-auto">
+          <div className="laptop:px px-0  laptop:ml-10 mx-0">
             <p className="text-[#25324B] mb-4 font-bold text-[1.5rem] leading-[1.8rem]">
               All Property
             </p>
-            <span className="flex laptop:justify-between w-full">
+            <span className="laptop:flex laptop:justify-between w-full mb-6">
               <p className="text-[#25324B] font-normal text-base leading-[1.6rem]">
                 Showing 73 results
               </p>
-              <p className="text-[#7C8493] ml-4 font-normal text-base leading-[1.6rem]">
-                Sort by:
-              </p>
-              <select className="pr-4 px-1 font-medium text-[#25324B]"
-                name="search-filter">
-                <option className="" value='Most relevant'>
-                  Most relevant
-                </option>
-                <option className="" value='Least relevant'>
-                  Least relevant
-                </option>
-                <option className="" value='most relevant'>
-                  Budget
-                </option>
-                <option className="" value='most relevant'>
-                  Location
-                </option>
-              </select>
+              <span className="flex mb-4 laptop:mb-0">
+                <label for="search-filter" className="text-[#7C8493] laptop:ml-4 font-normal text-base leading-[1.6rem]">
+                  Sort by:
+                </label>
+                <select id="search-filter" className="pr-4 px-1 font-medium text-[#25324B]"
+                  name="search-filter">
+                  <option className="" value='Most relevant'>
+                    Most relevant
+                  </option>
+                  <option className="" value='Least relevant'>
+                    Least relevant
+                  </option>
+                  <option className="" value='most relevant'>
+                    Budget
+                  </option>
+                  <option className="" value='most relevant'>
+                    Location
+                  </option>
+                </select>
+                <span className="flex laptop:ml-12">
+                  <span className="mx-2" title='Grid view'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                      <g clip-path="url(#clip0_1014_972)">
+                        <path d="M9 4.49998H5C4.44772 4.49998 4 4.9477 4 5.49998V9.49998C4 10.0523 4.44772 10.5 5 10.5H9C9.55228 10.5 10 10.0523 10 9.49998V5.49998C10 4.9477 9.55228 4.49998 9 4.49998Z" fill="#010886" stroke="#010886" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M19 4.49998H15C14.4477 4.49998 14 4.9477 14 5.49998V9.49998C14 10.0523 14.4477 10.5 15 10.5H19C19.5523 10.5 20 10.0523 20 9.49998V5.49998C20 4.9477 19.5523 4.49998 19 4.49998Z" fill="#010886" stroke="#010886" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M9 14.5H5C4.44772 14.5 4 14.9477 4 15.5V19.5C4 20.0523 4.44772 20.5 5 20.5H9C9.55228 20.5 10 20.0523 10 19.5V15.5C10 14.9477 9.55228 14.5 9 14.5Z" fill="#010886" stroke="#010886" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M19 14.5H15C14.4477 14.5 14 14.9477 14 15.5V19.5C14 20.0523 14.4477 20.5 15 20.5H19C19.5523 20.5 20 20.0523 20 19.5V15.5C20 14.9477 19.5523 14.5 19 14.5Z" fill="#010886" stroke="#010886" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_1014_972">
+                          <rect width="24" height="24" fill="white" transform="translate(0 0.5)" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </span>
+                  <span className="mx-2" title='List view'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                      <g opacity="0.5" clip-path="url(#clip0_1014_854)">
+                        <path d="M18 4.49998H6C4.89543 4.49998 4 5.39542 4 6.49998V8.49998C4 9.60455 4.89543 10.5 6 10.5H18C19.1046 10.5 20 9.60455 20 8.49998V6.49998C20 5.39542 19.1046 4.49998 18 4.49998Z" stroke="#515B6F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M18 14.5H6C4.89543 14.5 4 15.3954 4 16.5V18.5C4 19.6046 4.89543 20.5 6 20.5H18C19.1046 20.5 20 19.6046 20 18.5V16.5C20 15.3954 19.1046 14.5 18 14.5Z" stroke="#515B6F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_1014_854">
+                          <rect width="24" height="24" fill="white" transform="translate(0 0.5)" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </span>
+                </span>
+              </span>
 
               {/* <img src={DivideLine} className="" alt='' /> */}
 
-              <span className="mx-2" title='Grid view'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-                  <g clip-path="url(#clip0_1014_972)">
-                    <path d="M9 4.49998H5C4.44772 4.49998 4 4.9477 4 5.49998V9.49998C4 10.0523 4.44772 10.5 5 10.5H9C9.55228 10.5 10 10.0523 10 9.49998V5.49998C10 4.9477 9.55228 4.49998 9 4.49998Z" fill="#010886" stroke="#010886" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M19 4.49998H15C14.4477 4.49998 14 4.9477 14 5.49998V9.49998C14 10.0523 14.4477 10.5 15 10.5H19C19.5523 10.5 20 10.0523 20 9.49998V5.49998C20 4.9477 19.5523 4.49998 19 4.49998Z" fill="#010886" stroke="#010886" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M9 14.5H5C4.44772 14.5 4 14.9477 4 15.5V19.5C4 20.0523 4.44772 20.5 5 20.5H9C9.55228 20.5 10 20.0523 10 19.5V15.5C10 14.9477 9.55228 14.5 9 14.5Z" fill="#010886" stroke="#010886" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M19 14.5H15C14.4477 14.5 14 14.9477 14 15.5V19.5C14 20.0523 14.4477 20.5 15 20.5H19C19.5523 20.5 20 20.0523 20 19.5V15.5C20 14.9477 19.5523 14.5 19 14.5Z" fill="#010886" stroke="#010886" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_1014_972">
-                      <rect width="24" height="24" fill="white" transform="translate(0 0.5)" />
-                    </clipPath>
-                  </defs>
-                </svg>
-              </span>
-              <span className="mx-2" title='List view'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-                  <g opacity="0.5" clip-path="url(#clip0_1014_854)">
-                    <path d="M18 4.49998H6C4.89543 4.49998 4 5.39542 4 6.49998V8.49998C4 9.60455 4.89543 10.5 6 10.5H18C19.1046 10.5 20 9.60455 20 8.49998V6.49998C20 5.39542 19.1046 4.49998 18 4.49998Z" stroke="#515B6F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M18 14.5H6C4.89543 14.5 4 15.3954 4 16.5V18.5C4 19.6046 4.89543 20.5 6 20.5H18C19.1046 20.5 20 19.6046 20 18.5V16.5C20 15.3954 19.1046 14.5 18 14.5Z" stroke="#515B6F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_1014_854">
-                      <rect width="24" height="24" fill="white" transform="translate(0 0.5)" />
-                    </clipPath>
-                  </defs>
-                </svg>
-              </span>
             </span>
 
-            <div class="grid grid-cols-1 gap-6 laptop:grid-cols-3 tablet:grid-cols-2 laptop:gap-8">
+            <div class="grid grid-cols-1 gap-6 laptop:grid-cols-3 tablet:grid-cols-2 laptop:gap-4">
               {/* 01 */}
               <div class=" border-2 rounded-[1rem]">
                 <img src={currentSlide.image} className="w-full" alt="bungalow-1" />
@@ -835,6 +839,80 @@ const Listings = () => {
         </div>
         {/* end of listings */}
 
+        {/* pagination */}
+        <ol className="flex justify-center gap-1 mb-4 pb-8 text-xs font-medium">
+          <li>
+            <a
+              href="#"
+              className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
+            >
+              <span className="sr-only">Prev Page</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-3 w-3"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
+          </li>
+
+          <li>
+            <a
+              className="block h-8 w-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+            >
+              1
+            </a>
+          </li>
+
+          <li
+            className="block h-8 w-8 rounded border-blue-600 bg-blue-600 text-center leading-8 text-white"
+          >
+            2
+          </li>
+
+          <li>
+            <a
+              className="block h-8 w-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+            >
+              3
+            </a>
+          </li>
+
+          <li>
+            <a
+              className="block h-8 w-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+            >
+              4
+            </a>
+          </li>
+
+          <li>
+            <a
+              className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
+            >
+              <span className="sr-only">Next Page</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-3 w-3"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
+          </li>
+        </ol>
+        {/* end of pagination */}
 
 
 
