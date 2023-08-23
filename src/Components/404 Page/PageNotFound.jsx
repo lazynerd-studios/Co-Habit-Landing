b18dd927d8f5b8592454feabec117696d25fe0f1
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-23 09:52:02 +0100

    Create PageNotFound.jsx

---
+++ src/Components/404 Page/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <h1 className="">
+                PageNotFound
+            </h1>
+        </div>
+    )
+}
+
+export default PageNotFound
