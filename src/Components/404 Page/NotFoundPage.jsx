99bec42279880d3292d33caddc30259a7d528e69
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-23 09:58:01 +0100

    Create NotFoundPage.jsx

---
+++ src/Components/404 Page/NotFoundPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./styles.css"
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1 className=" text-4xl">
+                404 Page NotFound
+            </h1>
+        </div>
+    )
+}
+
+export default NotFoundPage
