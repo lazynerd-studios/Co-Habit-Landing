ac1013332d2495801ed2d21afbec9f923fa0e9c6
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-24 13:17:49 +0100

    Create SideBar-old.js

---
+++ src/Components/SideBar/SideBar-old.js
@@ -0,0 +1,273 @@
+<div className='laptop:block min-w-fit hidden'>
+    {/* 01 */}
+    <section className='mb-10'>
+        <span className="flex font-semibold justify-between text-[1rem] leading-[1.6rem]">
+            <p className="mb-5">
+                Property Type
+            </p>
+            <span className="py-1">
+                <BsChevronUp />
+            </span>
+        </span>
+
+        <form className='select-none'>
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="apartment"
+                    name=""
+                    value=""
+                    size={30}
+                    className="" />
+                <label for="apartment"
+                    className="text-[#515B6F] px-4">
+                    Apartment (7,997)
+                </label>
+            </span>
+
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="duplex"
+                    name=""
+                    value=""
+                    size={30}
+                    className="" />
+                <label for="duplex"
+                    className="text-[#515B6F] px-4">
+                    Duplex (5)
+                </label>
+            </span>
+
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="mini-flat"
+                    name=""
+                    value=""
+                    size={30}
+                    className="" />
+                <label for="mini-flat"
+                    className="text-[#515B6F] px-4">
+                    Mini-Flat(2)
+                </label>
+            </span>
+
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="bungalow"
+                    name=""
+                    value=""
+                    size={30}
+                    className="" />
+                <label for="bungalow"
+                    className="text-[#515B6F] px-4">
+                    Bungalow (24)
+                </label>
+            </span>
+
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="workspace"
+                    name=""
+                    value=""
+                    size={30}
+                    className="" />
+                <label for="workspace"
+                    className="text-[#515B6F] px-4">
+                    Workspace (3)
+                </label>
+            </span>
+        </form>
+    </section>
+
+    {/* 02 */}
+    <section className='mb-10'>
+        <span className="flex font-semibold justify-between text-[1rem] leading-[1.6rem]">
+            <p className="mb-5">
+                Location
+            </p>
+            <span className="py-1">
+                <BsChevronUp />
+            </span>
+        </span>
+
+        <form className='select-none'>
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="abuja"
+                    name=""
+                    value=""
+                    size={30}
+                    className=""
+                    checked />
+                <label for="abuja"
+                    className="text-[#515B6F] px-4">
+                    Abuja(24)
+                </label>
+            </span>
+
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="lagos"
+                    name=""
+                    value=""
+                    size={30}
+                    className="" />
+                <label for="lagos"
+                    className="text-[#515B6F] px-4">
+                    Lagos(3)
+                </label>
+            </span>
+        </form>
+    </section>
+
+    {/* 03 */}
+    <section className='mb-10'>
+        <span className="flex font-semibold justify-between text-[1rem] leading-[1.6rem]">
+            <p className="mb-5">
+                Areas
+            </p>
+            <span className="py-1">
+                <BsChevronUp />
+            </span>
+        </span>
+
+        <form className='select-none'>
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="abaji"
+                    name=""
+                    value=""
+                    size={30}
+                    className="" />
+                <label for="abaji"
+                    className="text-[#515B6F] px-4">
+                    Abaji (57)
+                </label>
+            </span>
+
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="bwari"
+                    name=""
+                    value=""
+                    size={30}
+                    className="" />
+                <label for="bwari"
+                    className="text-[#515B6F] px-4">
+                    Bwari (3)
+                </label>
+            </span>
+
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="Gwagwalada"
+                    name=""
+                    value=""
+                    size={30}
+                    className=""
+                />
+                <label for="Gwagwalada"
+                    className="text-[#515B6F] px-4">
+                    Gwagwalada(5)
+                </label>
+            </span>
+
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="kuje"
+                    name=""
+                    value=""
+                    size={30}
+                    className=""
+                    checked />
+                <label for="kuje"
+                    className="text-[#515B6F] px-4">
+                    Kuje (12)
+                </label>
+            </span>
+
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="kwali"
+                    name=""
+                    value=""
+                    size={30}
+                    className="" />
+                <label for="kwali"
+                    className="text-[#515B6F] px-4">
+                    Kwali (8)
+                </label>
+            </span>
+        </form>
+    </section>
+
+    {/* 04 */}
+    <section className='mb-10'>
+        <span className="flex font-semibold justify-between text-[1rem] leading-[1.6rem]">
+            <p className="mb-5">
+                Budget
+            </p>
+            <span className="py-1">
+                <BsChevronUp />
+            </span>
+        </span>
+
+        <form className='select-none'>
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="100-500k"
+                    name=""
+                    value=""
+                    size={30}
+                    className="" />
+                <label for="100-500k"
+                    className="text-[#515B6F] px-4">
+                    100k -500k (4)
+                </label>
+            </span>
+
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="600-900k"
+                    name=""
+                    value=""
+                    size={30}
+                    className="" />
+                <label for="600-900k"
+                    className="text-[#515B6F] px-4">
+                    600 - 900k (6)
+                </label>
+            </span>
+
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="1-2m"
+                    name=""
+                    value=""
+                    size={30}
+                    className="" />
+                <label for="1-2m"
+                    className="text-[#515B6F] px-4">
+                    1m - 2m (10)
+                </label>
+            </span>
+
+            <span className='block mb-5'>
+                <input type="checkbox"
+                    id="3m-above"
+                    name=""
+                    value=""
+                    size={30}
+                    className=" checked:bg-[#010886]"
+                    checked
+                />
+
+                <label for="3m-above"
+                    className="text-[#515B6F] px-4">
+                    3m or above (4)
+                </label>
+
+            </span>
+        </form>
+    </section>
+
+</div>
