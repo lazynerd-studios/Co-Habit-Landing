c62b921445453d43f8526860be8d137d3ef230b5
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-24 12:27:15 +0100

    Update NotFoundPage.jsx

--- src/Components/404 Page/NotFoundPage.jsx
+++ src/Components/404 Page/NotFoundPage.jsx
@@ -12,8 +12,8 @@ const NotFoundPage = () => {
                 The page you are looking for does not exist.
             </p> */}
 
-            <section className="page_404 mx-[5rem]">
-                <div className="container">
+            <section className="page_404 min-h-screen bg-[#f8f8fd] ">
+                <div className="container laptop:mx-[6rem] tablet:mx-[3rem]">
                     <div className="row">
                         <div className="col-sm-12 ">
                             <div className="col-sm-10 col-sm-offset-1  text-center">
@@ -21,14 +21,16 @@ const NotFoundPage = () => {
                                     <h1 className="text-center ">404</h1>
                                 </div>
 
-                                <div className="contant_box_404">
+                                <div className="contant_box_404 tablet:mt-4">
                                     <h3 className="h2">
                                         Look like you're lost
                                     </h3>
 
-                                    <p>the page you are looking for not avaible!</p>
+                                    <p className="">
+                                        the page you are looking for not avaible!
+                                    </p>
 
-                                    <Link to="/" className="btn text-white bg-[#010886] border-none hover:bg-[#010886]/75 ">Go to Home</Link>
+                                    <Link to="/" className="btn text-white bg-[#010886] mt-2 border-none hover:bg-[#010886]/75 ">Go to Home</Link>
                                 </div>
                             </div>
                         </div>
