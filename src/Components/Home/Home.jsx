7e5a7d48d774b7933d2bdbd168b4c93f317643e8
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-27 01:31:48 +0100

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -480,7 +480,6 @@ const Home = () => {
             </div>
 
 
-
             <button
               className="btn my-6 normal-case laptop:mx-[26rem] tablet:mx-[19rem] mx-[5rem] text-[#010886] font-bold leading-[1.8rem] rounded-none border border-[#B8C9C9] bg-[#e7faf4e5] hover:bg-[#E7FAF4] hover:border">
               Browse More locations
