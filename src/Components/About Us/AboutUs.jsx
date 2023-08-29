25cb43ba0341214cfb9049514484ffd973ec44c0
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-29 10:39:46 +0100

    Update AboutUs.jsx

--- src/Components/About Us/AboutUs.jsx
+++ src/Components/About Us/AboutUs.jsx
@@ -22,12 +22,12 @@ const AboutUs = () => (
     >
       {/* <div className='min-h-screen'> */}
       <div className="hero place-content-start ">
-        <div className="hero-content w-full mt-10 mb-8  bg-[#010886] laptop:mt- laptop:mx-[8rem]">
+        <div className="hero-content w-full mt-10 mb-12 h-[16.1875rem] bg-[#010886] laptop:mt- laptop:mx-[8rem]">
           {/* <div className="max-w-lg"> */}
           <div className="w-full laptop:mx-[8rem] top-0 laptop:mt-[3rem]">
             <img src={BreadCrumb} className='' alt="Breadcrumb" />
             <h1
-              className="text-[3rem] leading-[3.4375rem] font-bold text-[#fff] mt-12">
+              className="text-[3rem] leading-[3.4375rem] font-bold text-[#fff] mt-8">
               About Us
             </h1>
           </div>
@@ -35,23 +35,22 @@ const AboutUs = () => (
         </div>
       </div>
 
-      <div className='bg-white p-4 absolute w-auto hidden laptop:block -mt-9 h-auto mx-[8rem] shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+      <div className='bg-white px-6 py-12 absolute w-auto hidden laptop:block -mt-[4.5rem] h-auto mx-[8rem] shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
         {/* <div className='grid grid-cols-2 gap-0'> */}
         <div class="flex justify-between">
 
-
-          <span className='px-8 border-2 py-0 h-12 rounded-full w-full'>
-            <button className='py-2'>
-              Hi there
+          <span className='px-8 border-2 py-0 h-12 rounded-[6.25rem] text-[#999999] border-[#999999] w-[35%] max-w-[40%] mr-10'>
+            <button className='py-2 font-bold text-[1rem] leading-[1.6rem]'>
+              EXPLORE WITH CO-HABIT
             </button>
           </span>
 
-          <div className='px-4'>
-            <h1 className=''>
+          <div className='px-4 w-full '>
+            <h1 className='text-[#1A1A1A] text-[2.5rem] font-bold leading-[2.75rem] mb-4'>
               OUR STORY
             </h1>
 
-            <p className='text-[1rem]'>
+            <p className='text-[1rem] text-[#7C8493] leading-[1.6rem]'>
               A ground-breaking platform that transforms the way
               we think about living arrangements is Co-Habit.
               In addition to simplifying the apartment search process,
@@ -74,15 +73,19 @@ const AboutUs = () => (
       </div>
       {/* </div> */}
 
-      <div className='h-[10rem] bg-[#EFEFEF]'>
+      <div className='h-[30rem] bg-[#EFEFEF]'>
 
       </div>
 
       <div className='py-8 bg-[#010886] text-white'>
         <div className='laptop:mx-[8rem]'>
-          <h2>
-            BENEFIT OF USING CO-HABIT
-          </h2>
+          <span className='px-8 border-2 py-4 rounded-[6.25rem] border-[#999999] w-[35%] max-w-[40%] mr-10'>
+            <button className='py-2 font-bold text-[1rem] leading-[1.6rem]'>
+              BENEFIT OF USING CO-HABIT
+            </button>
+          </span>
+
+
         </div>
       </div>
 
