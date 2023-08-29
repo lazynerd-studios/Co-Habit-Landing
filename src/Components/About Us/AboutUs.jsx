7079e2434191709167752a4aa7bb28979bbcef0b
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-29 13:58:26 +0100

    Update AboutUs.jsx

--- src/Components/About Us/AboutUs.jsx
+++ src/Components/About Us/AboutUs.jsx
@@ -212,7 +212,7 @@ const AboutUs = () => (
       <div className='py-8 bg-[#EFEFEF] h-auto text-[#333333]'>
         <div className='laptop:mx-[8rem] laptop:pt-4'>
           <span className='px-8 border-2 py-4 rounded-[6.25rem] border-[#999999] w-[35%] max-w-[40%] mr-10'>
-            <button className='py-2 font-bold text-[1rem] leading-[1.6rem]'>
+            <button className='py-2 mb-6 font-bold text-[1rem] leading-[1.6rem]'>
               OUR STORY
             </button>
           </span>
