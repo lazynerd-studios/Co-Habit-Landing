19ce97666d8aa02f62d7833168ad038437270119
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-04 15:42:42 +0100

    Create DetailsPage.jsx

---
+++ src/Pages/DetailsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Details from "../Components/Details/Details"
+
+const DetailsPage = () => {
+  return (
+    <div>
+      <Details />
+    </div>
+  )
+}
+
+export default DetailsPage
