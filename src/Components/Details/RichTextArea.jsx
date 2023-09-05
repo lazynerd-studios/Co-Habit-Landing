7c7a1d731d6b88f49b20900b1884825fae0d02da
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-05 15:50:18 +0100

    Create RichTextArea.jsx

---
+++ src/Components/Details/RichTextArea.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+const RichTextArea = () => {
+    const [text, setText] = useState('');
+    const [showEmojiPanel, setShowEmojiPanel] = useState(false);
+
+    const handleBoldClick = () => {
+        setText((prevText) => `<b>${prevText}</b>`);
+    };
+
+    const handleItalicClick = () => {
+        setText((prevText) => `<i>${prevText}</i>`);
+    };
+
+    const handleParagraphClick = () => {
+        setText((prevText) => `${prevText}\n\n`);
+    };
+
+    const handleBulletPointsClick = () => {
+        setText((prevText) => `${prevText}\n• `);
+    };
+
+    const handleAttachmentClick = () => {
+        // Implement attachment logic here (e.g., opening a file picker)
+    };
+
+    return (
+        <div>
+            <div>
+                <button onClick={handleBoldClick}>B</button>
+                <button onClick={handleItalicClick}>I</button>
+                <button onClick={handleParagraphClick}>¶</button>
+                <button onClick={handleBulletPointsClick}>•</button>
+                <button onClick={() => setShowEmojiPanel(!showEmojiPanel)}>😀</button>
+                <button onClick={handleAttachmentClick}>🔗</button>
+            </div>
+            <textarea
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                rows={4}
+                cols={50}
+            />
+        </div>
+    );
+};
+
+export default RichTextArea;
