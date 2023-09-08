b1e243d94f83ec582d43e7b47cf60b4665d4c3bc
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-08 18:06:35 +0100

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -75,11 +75,11 @@ const Home = () => {
                 <span className="mb-6 relative">
                   <img src={HeroImage} className="laptop:max-w-[75rem] hidden tablet:flex laptop:flex laptop:mb-8 z-10 w-full mx-auto" alt="Hero Image" />
 
-                  <Link to="#reviews" className="">
+                  <a href="#reviews" className="">
                     {/* come back here to make the icon center on wider devices */}
                     {/* wider:mx-[32.4rem] laptop:mx-[31.3rem] */}
                     <img src={ScrollToExplore} className="laptop:max-w-[3.9rem] mx-auto -mt-[6rem] z-10 hidden laptop:flex" alt="scroll-to-explore" />
-                  </Link>
+                  </a>
                 </span>
                 <img src={MobileLines} className="laptop:hidden tablet:hidden" alt="Mobile lines" />
 
