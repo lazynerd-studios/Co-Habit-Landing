73af8f66716b4b3a6de9aec1ee2257bb092b4822
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-19 10:50:17 +0100

    Update slides.jsx

--- src/Components/Slideshow/slides.jsx
+++ src/Components/Slideshow/slides.jsx
@@ -33,31 +33,31 @@ export const slides = [
         baths: "2 Baths",
         area: "Ikoyi, Lagos"
     },
-    {
-        image: Bungalow1,
-        house: "Bungalow",
-        price: "NGN150,000",
-        measurement: "1400sqft",
-        rooms: "3 Rooms",
-        baths: "3 Baths",
-        area: "Ikeja, Lagos"
-    },
-    {
-        image: Bungalow2,
-        house: "Bungalow",
-        price: "NGN150,000",
-        measurement: "1200sqft",
-        rooms: "2 Rooms",
-        baths: "2 Baths",
-        area: "Epe, Lagos"
-    },
-    {
-        image: Bungalow3,
-        house: "Bungalow",
-        price: "NGN150,000",
-        measurement: "1500sqft",
-        rooms: "3 Rooms",
-        baths: "3 Baths",
-        area: "Apapa, Lagos"
-    },
+    // {
+    //     image: Bungalow1,
+    //     house: "Bungalow",
+    //     price: "NGN150,000",
+    //     measurement: "1400sqft",
+    //     rooms: "3 Rooms",
+    //     baths: "3 Baths",
+    //     area: "Ikeja, Lagos"
+    // },
+    // {
+    //     image: Bungalow2,
+    //     house: "Bungalow",
+    //     price: "NGN150,000",
+    //     measurement: "1200sqft",
+    //     rooms: "2 Rooms",
+    //     baths: "2 Baths",
+    //     area: "Epe, Lagos"
+    // },
+    // {
+    //     image: Bungalow3,
+    //     house: "Bungalow",
+    //     price: "NGN150,000",
+    //     measurement: "1500sqft",
+    //     rooms: "3 Rooms",
+    //     baths: "3 Baths",
+    //     area: "Apapa, Lagos"
+    // },
 ];
