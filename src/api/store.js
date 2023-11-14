5ad4b69190d325cdf114db6ed7673baa6a03866e
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2023-11-14 11:00:38 +0100

    modified store config to not show in production

--- src/api/store.js
+++ src/api/store.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { configureStore } from "@reduxjs/toolkit";
 import { listingApi } from "./listingApi";
 
@@ -5,6 +6,7 @@ export const store = configureStore({
   reducer: {
     [listingApi.reducerPath]: listingApi.reducer,
   },
+  devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({}).concat([listingApi.middleware]),
 });
